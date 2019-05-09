cef4cf7077b63455873126c35a46667b4b0a3332
abhisheksarkar30 <abhisheksarkar30@gmail.com>
2019-05-09 13:03:31 +0530

    Fixing launchTomcat.bat

--- Windows-Batches/launchTomcat.bat
+++ Windows-Batches/launchTomcat.bat
@@ -4,7 +4,8 @@ set /P sw=Enter r:replace with current war only:
 if not "%sw%"=="r" (call build clean install -Dmaven.test.skip -Dall -Drest -Ddeployments=gv-th)
 if exist gv-deployments\gv-dist\target\igv-tomcat.war (
 del %CATALINA_HOME%\webapps\igv-*.war
-rd /s/q %CATALINA_HOME%\webapps\igv-*
+rd /s/q %CATALINA_HOME%\webapps\igv-tomcat
+rd /s/q %CATALINA_HOME%\webapps\igv-rest
 copy gv-deployments\gv-dist\target\igv-tomcat.war %CATALINA_HOME%\webapps
 del E:\apache-tomcat-7.0.57\webapps\igv-tomcat\WEB-INF\classes\log4j.properties
 copy "C:\Users\abhisheksa\Desktop\Batch-Files\Exchange-Files\log4j-debug.properties" E:\apache-tomcat-7.0.57\webapps\igv-tomcat\WEB-INF\classes\log4j.properties
