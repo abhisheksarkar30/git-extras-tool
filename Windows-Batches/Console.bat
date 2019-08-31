5f0dc2a22baabe7fe09341347e99ea09e324686b
Abhishek Sarkar <abhisheksarkar30@gmail.com>
2019-08-31 09:50:53 +0530

    Deleted outdated batches

--- Windows-Batches/Console.bat
+++
@@ -1,6 +0,0 @@
-if exist E:\igv-console (
-start cmd -new_console /k E:\igv-console\launch ServiceController
-timeout 5 > NUL
-start cmd -new_console /k E:\igv-console\launch REF/StartReportRunner
-)
-pause
