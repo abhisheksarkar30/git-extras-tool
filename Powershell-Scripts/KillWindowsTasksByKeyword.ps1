e37d835de0892828099b667c8fad00d5c40c47f8
Abhishek Sarkar <abhisheksarkar30@gmail.com>
2019-09-28 13:05:34 +0530

    gite tool migration

--- Powershell-Scripts/KillWindowsTasksByKeyword.ps1
+++
@@ -1,9 +0,0 @@
-﻿$keyword = Read-Host -Prompt 'Enter keyword:'
-$javaActive = jps -l | select-string $keyword
-for($i = 0; $i -lt $javaActive.length; $i++) { 
-    Write-host $javaActive[$i]
-    $targetPid = $javaActive[$i].ToString().Substring(0,$javaActive[$i].ToString().IndexOf(" "))
-    $result = taskkill /f /pid $targetPid
-    Write-Host $result
-}
-pause
