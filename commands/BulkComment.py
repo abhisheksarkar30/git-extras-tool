1db2eb57830f141dc4a6d79e80ec5c413d283585
Abhishek Sarkar <abhisheksarkar30@gmail.com>
2020-05-04 17:31:05 +0530

    GITE-1 : Bulk comment feature added

--- commands/BulkComment.py
+++ commands/BulkComment.py
@@ -23,40 +23,33 @@ def load_props():
 class Command(AbstractCommand):
 
     def get_command_code(self):
-        return "pushc"
+        return "comment"
 
     def get_command_desc(self):
-        return "Push and comment"
+        return "Bulk jira comment"
 
     def add_options(self, parser):
         # Add applicable arguments and parse
-        parser.add_argument("-f", help="Forced push", action='store_true')
+        parser.add_argument("-c", help="Commit IDs comma separated", required=True)
         return
 
     # Entry to command execution
     def main(self, args):
-        commit_list = Utils.execute_command("git cherry -v").split("\n")
+        commit_list = args.c.split(",")
         keys = load_props()
         jira_base = keys['jira.url']
         jira_cred = keys['jira.user'] + ":" + keys['jira.pass']
         base_comment = "==============Automated Comment==============\n"
         base_comment += "Repository : " + Utils.execute_command("git config --get remote.origin.url") + "\n"
         base_comment += "Branch : " + Utils.execute_command("git branch --show-current") + "\n"
-        result = Utils.execute_command("git push" if len(sys.argv) == 1 else "git push -f")
-        print(result)
-        # Checking for successful push to remote
-        if result.find("rejected") != -1 or result.find("exit code 1") != -1:
-            print("Push failed")
-            return
         for commit in commit_list:
-            print("\n" + commit)
-            comment = base_comment + "Commit Details :\n" + commit + "\n" + "Related files :\n"
-            first_space_index = commit.find(" ")
-            second_space_index = commit.find(" ", first_space_index + 1)
-            commit_id = commit[first_space_index + 1: second_space_index]
-            commit_msg = commit[second_space_index + 1:].strip()
-            if commit_msg.find(":") == -1:
+            commit_id = commit.strip()
+            print("\n" + commit_id)
+            commit_msg = Utils.execute_command("git show-branch --no-name " + commit_id)
+            if commit_msg.find("fatal") != -1:
+                print(commit_msg)
                 continue
+            comment = base_comment + "Commit Details :\n" + commit_id + " " + commit_msg + "\n" + "Related files :\n"
             jira_id = commit_msg[: commit_msg.find(":")].strip()
             jira_url = jira_base + "rest/api/2/issue/" + jira_id + "/comment"
             comment = (comment + Utils.execute_command("git diff-tree --no-commit-id --name-status -r " +
